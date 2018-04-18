299cbf53bbc57c299efc9f20bd7ca2b0e8e12cd0
mariireynoso <fernando6reynoso@hotmail.com>
2018-04-18 17:40:40 -0300

    guia herenc

---
+++ GUIA 1/Albunes.py
@@ -0,0 +1,43 @@
+class Album(object):
+    titulo_album=None
+
+    def __init__(self):
+        self.lista_de_canciones=[]
+
+    def AgregarAlbum (self,titulo_album):
+        self.titulo_album=titulo_album
+
+
+class Cancion(object):
+    titulo_cancion=None
+
+    def __init__(self):
+        self.lista_artista=[]
+    def __init__(self):
+        self.lista_autores=[]
+
+    def AgregarCancion (self, titulo_cancion):
+        self.titulo_cancion=titulo_cancion
+
+    def AgregarArtista(self,lista_artista):
+        self.lista_artista.append = lista_artista[]
+
+    def AgregarAutor (self,lista_autores):
+        self.lista_autores.append=lista_autores[]
+
+
+
+
+
+class Persona(object):
+    nombre=None
+    apellido=None
+    fecha_nac=None
+
+class Artistas(Persona):
+    pass
+
+class Autores(Persona):
+    nacionalidad=None
+
+
---
+++ GUIA 1/main.py
@@ -0,0 +1,8 @@
+from Albunes import Album
+from Albunes import Cancion
+from Albunes import Personas
+from Albunes import Artistas
+from Albunes import Autores
+
+
+a()= Album
